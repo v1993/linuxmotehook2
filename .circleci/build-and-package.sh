1092397e0acc51b172a03686eb827ec504fe5de2
v1993 <v19930312@gmail.com>
2022-01-18 18:36:33 +0300

    Fix to CI

--- .circleci/build-and-package.sh
+++ .circleci/build-and-package.sh
@@ -1,35 +1,47 @@
 #!/bin/bash
 set -e
 
+mkdir /tmp/persist
+mkdir /tmp/persist/artifacts
+
 # Step 1 - standalone build
 echo "Compiling and installing"
 
-meson --buildtype=release -Db_lto=true --prefix=/tmp/prefix /tmp/build
-ninja -C /tmp/build -j4
-ninja -C /tmp/build install
+meson --buildtype=release -Db_lto=true --prefix=/tmp/prefix /tmp/persist/build
+ninja -C /tmp/persist/build -j4
+ninja -C /tmp/persist/build install
 
 echo "Creating bundle"
 
-mkdir /tmp/artifacts
-
-tar -pcvz --transform 's,^,linuxmotehook2/,' --transform 's,/tmp/prefix,,' -f  /tmp/artifacts/linuxmotehook2-amd64.tar.gz \
+tar -pcvz --transform 's,^,linuxmotehook2/,' --transform 's,/tmp/prefix,,' -f  /tmp/persist/artifacts/linuxmotehook2-amd64.tar.gz \
 /tmp/prefix \
 README.md LICENSE
+
+# Stop if we're not on main branch
+
+[[ $CIRCLE_BRANCH == main ]] || exit
 
 # Step 2 - deb build
 
 echo "Importing launchpad key"
 
-printf '%s' "$(LAUNCHPAD_KEY_BASE64)" | base64 --decode | gpg --import
+printf '%s' "$LAUNCHPAD_KEY_BASE64" | base64 --decode | gpg --import --batch
 
 echo "Creating changelog"
 
-MAIN_VERSION=$(meson introspect /tmp/build --projectinfo | jq -r .version)
+MAIN_VERSION=$(meson introspect /tmp/persist/build --projectinfo | jq -r .version)
 DEBFULLNAME=v1993 DEBEMAIL=v19930312@gmail.com dch --create --package=linuxmotehook2 --newversion="${MAIN_VERSION}.$(date -u +%Y%m%d%H%M%S)" --distribution unstable -- 'New upstream commit'
 
 echo "Building deb package"
 
-debuild -S -k"$LAUNCHPAD_KEY_ID"
+echo "" > key_password
+
+debuild -S -uc -us
+debsign \
+  -p"echo '' | gpg --batch -v --pinentry-mode loopback --passphrase-fd=0"\
+  -S -k"$LAUNCHPAD_KEY_ID" ../*.changes
+
+rm -f key_password
 
 echo "Uploading to launchpad"
 
