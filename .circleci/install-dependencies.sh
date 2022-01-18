5b0757f0e79a61f9536a5c3c56dc87daedaec7d7
v1993 <v19930312@gmail.com>
2022-01-18 12:42:54 +0300

    CI: fix deps installation, 1

--- .circleci/install-dependencies.sh
+++ .circleci/install-dependencies.sh
@@ -4,7 +4,11 @@ set -e
 export DEBIAN_FRONTEND="noninteractive"
 export TZ="Europe/London"
 
-apt-get install build-essentials \
+apt-get install -y software-properties-common
+
+add-apt-repository -y ppa:vala-team/next
+
+apt-get install -y build-essentials \
     libudev-dev zlib1g-dev \
     valac libgee-0.8-dev \
     meson
