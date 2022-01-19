c721310edea442b79d34070309d047a8ac4ae0a5
v1993 <v19930312@gmail.com>
2022-01-19 17:04:57 +0300

    CI fixups

--- .circleci/install-dependencies.sh
+++ .circleci/install-dependencies.sh
@@ -6,13 +6,13 @@ export TZ="Etc/UTC"
 
 apt-get install -y software-properties-common
 
-add-apt-repository -y -P ppa:vala-team/next
+add-apt-repository -y ppa:vala-team/next
 
-[[ $(lsb_release -cs) != focal ]] || add-apt-repository -y -P ppa:savoury1/build-tools
+[[ $(lsb_release -cs) != focal ]] || add-apt-repository -y ppa:savoury1/build-tools
 
 apt-get install -y build-essential \
     libudev-dev zlib1g-dev \
     valac libgee-0.8-dev \
     meson \
     \
-    devscripts gpg dput-ng jq basez debhelper
+    devscripts gpg dput-ng jq basez debhelper distro-info
