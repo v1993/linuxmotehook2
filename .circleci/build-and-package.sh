ddd76afc160a6096cd2bc7fde860861cec563686
v1993 <v19930312@gmail.com>
2022-01-19 18:52:59 +0300

    CI: remove references to persisting
    We don't use it anyways

--- .circleci/build-and-package.sh
+++ .circleci/build-and-package.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -e
 
-mkdir /tmp/persist
-mkdir /tmp/persist/artifacts
+mkdir /tmp/artifacts
 
 echo "Compiling and installing"
 
-meson --buildtype=release -Db_lto=true --prefix=/tmp/prefix /tmp/persist/build
-ninja -C /tmp/persist/build -j4
-ninja -C /tmp/persist/build install
+meson --buildtype=release -Db_lto=true --prefix=/tmp/prefix /tmp/build
+ninja -C /tmp/build -j4
+ninja -C /tmp/build install
 
 echo "Creating bundle"
 
-tar -pcvz --transform 's,^,linuxmotehook2/,' --transform 's,/tmp/prefix,,' -f  /tmp/persist/artifacts/linuxmotehook2-amd64.tar.gz \
+tar -pcvz --transform 's,^,linuxmotehook2/,' --transform 's,/tmp/prefix,,' -f  /tmp/artifacts/linuxmotehook2-amd64.tar.gz \
 /tmp/prefix \
 README.md LICENSE
