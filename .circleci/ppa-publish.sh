462cd5b882425f60b722d37d478ba1d127075434
v1993 <v19930312@gmail.com>
2022-01-19 17:51:16 +0300

    Specify distro in package version

--- .circleci/ppa-publish.sh
+++ .circleci/ppa-publish.sh
@@ -13,7 +13,9 @@ printf '%s' "$LAUNCHPAD_KEY_BASE64" | base64 --decode | gpg --import --batch
 
 echo "Creating changelog"
 
-DEBFULLNAME=v1993 DEBEMAIL=v19930312@gmail.com dch --create --package=linuxmotehook2 --newversion="$2" --distribution $(lsb_release -cs) -- "$3"
+DISTRO=$(lsb_release -cs)
+
+DEBFULLNAME=v1993 DEBEMAIL=v19930312@gmail.com dch --create --package=linuxmotehook2 --newversion="${2}~${DISTRO}" --distribution $DISTRO -- "$3"
 
 echo "Building deb package"
 
