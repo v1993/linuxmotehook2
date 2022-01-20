709a8c7389251f96b09bc19dd1667c7f0b23c7b7
v1993 <v19930312@gmail.com>
2022-01-20 09:46:29 +0300

    More CI fixes

--- .circleci/build-and-package.sh
+++ .circleci/build-and-package.sh
@@ -3,13 +3,9 @@ set -e
 
 mkdir /tmp/artifacts
 
-echo "Compiling and installing"
-
 meson --buildtype=release -Db_lto=true --prefix=/tmp/prefix /tmp/build
 ninja -C /tmp/build -j4
 ninja -C /tmp/build install
-
-echo "Creating bundle"
 
 tar -pcvz --transform 's,^,linuxmotehook2/,' --transform 's,/tmp/prefix,,' -f  /tmp/artifacts/linuxmotehook2-amd64.tar.gz \
 /tmp/prefix \
